6eebfd1975fa0f20bb73c01b035d3de2e62978be
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-09-29 14:59:19 -0400

    respaced links

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -15,13 +15,13 @@ const Navbar = () => {
                 HOME |
             </Link>
             <Link href='https://minecraft3clone-g8o6.vercel.app/'>
-                Minecraft React/3js Clone (Desktop Only) |
+                 Minecraft React/3js Clone (Desktop Only) |
             </Link>
             <Link href='https://effervescent-yeot-a07484.netlify.app/'>
-                Bottle Express (Video Chat app) |
+                 Bottle Express (Video Chat app) |
             </Link>
             <Link href='https://account.venmo.com/u/figgsboson'>
-                SUPPORT MY WORK
+                 SUPPORT MY WORK
             </Link>
         </p>
 
